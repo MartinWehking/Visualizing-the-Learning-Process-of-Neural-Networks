b5858f63a12607140233be8e1ef99a0ba0f43241
Martin Wehking <wehkin02@gw.uni-passau.de>
2018-02-16 17:06:19 +0100

    Accuracy of the neural net is very good now. Some fixes. Changes in the UI. UI sub-selection of neurons does not work yet.

--- neural_net.py
+++ neural_net.py
@@ -3,7 +3,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from sklearn.preprocessing import normalize
+import random as rd
 from sklearn.cluster import DBSCAN
 
 
@@ -14,46 +14,56 @@ class ObservableNet:
         self.gradients = pd.DataFrame(columns=['gradient', 'epoch', 'layer'])
         self.weights = pd.DataFrame(columns=['weight', 'epoch', 'layer'])
 
-    def add_layer(self, units, name, type='dense'):
+    def add_layer(self, units, name, type='dense', activation='relu'):
         if type == 'dense':
-            self.previous_input = tf.layers.dense(self.previous_input, units, tf.nn.relu, name=name)
+            if activation == 'relu':
+                self.previous_input = tf.layers.dense(self.previous_input, units, tf.nn.relu, name=name)
+            else:
+                self.previous_input = tf.layers.dense(self.previous_input, units, name=name)
 
     def create_net(self, learning_rate):
         y_ = tf.placeholder(tf.int32)
+        y = tf.one_hot(y_, depth=10)
 
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.previous_input,
-                                                                         labels=y_))
+                                                                         labels=y))
         optimizer = tf.train.AdamOptimizer(learning_rate)
         gradients = optimizer.compute_gradients(loss)
         apply_operation = optimizer.apply_gradients(gradients)
 
-        correct_prediction = tf.equal(tf.argmax(self.previous_input, axis=1), tf.argmax(y_, axis=1))
+        correct_prediction = tf.equal(tf.argmax(self.previous_input, axis=1), tf.argmax(y, axis=1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
         return gradients, apply_operation, y_, accuracy
 
-    def train(self, epochs=5, learning_rate=0.0001):
-        mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    def train(self, epochs=2, learning_rate=0.001):
+
+        mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+        complete_train_data = mnist.train.images  # Returns np.array
+        complete_train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+        eval_data = mnist.test.images  # Returns np.array
+        eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
 
         gradients, apply_operation, y_, accuracy = self.create_net(learning_rate)
 
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
+            indices = [index for index in range(55000)]
             for epoch in range(epochs):
                 print('Starting epoch: ' + str(epoch))
                 save_grad_weights = []
+                rd.shuffle(indices)
+                # sess.run([train_image_batch, train_label_batch])
                 for i in range(50):
-                    # ToDO Better method for shuffling
-                    batch = mnist.train.next_batch(1000)
-                    batch_train, batch_label = batch[0], batch[1]
-                    grad_weights = sess.run(gradients, feed_dict={self.first_input: batch_train, y_: batch_label})
-                    save_grad_weights = [np.copy(grad_weight) for i, grad_weight in enumerate(grad_weights) if
+                    train_indices = indices[i * 1000: (i + 1) * 1000]
+                    train_data = [complete_train_data[i] for i in train_indices]
+                    train_labels = [complete_train_labels[i] for i in train_indices]
+                    grad_weights, s = sess.run([gradients, apply_operation], feed_dict={self.first_input: train_data, y_:
+                        train_labels})
+                    save_grad_weights = [grad_weight for i, grad_weight in enumerate(grad_weights) if
                                          i % 2 == 0]
                     self.add_gradients(save_grad_weights)
-                    sess.run(apply_operation, feed_dict={self.first_input: batch_train, y_: batch_label})
-                test = mnist.test.next_batch(9999)
-                test_data, test_labels = test[0], test[1]
-                print('testing ' + str(accuracy.eval(feed_dict={self.first_input: test_data, y_: test_labels})))
+                print('testing ' + str(accuracy.eval(feed_dict={self.first_input: eval_data, y_: eval_labels})))
                 self.save_weights(save_grad_weights, epoch)
                 self.save_gradients(epoch)
 
@@ -93,13 +103,15 @@ class ObservableNet:
 
 
 def normalize_time_vectors(time_vectors):
+    #ToDo Very Slow. Needs to be changed.
     normalized_time_vectors = list()
     for layer in time_vectors:
         normalized_layer = list()
         for row in layer:
-            normalized_row = list()
-            for time_vector in row:
-                normalized_row.append(normalize_time_vector(time_vector))
+            #normalized_row = list()
+            #for time_vector in row:
+            #    normalized_row.append(normalize_time_vector(time_vector))
+            normalized_layer.append(np.apply_along_axis())
             normalized_layer.append(np.array(normalized_row))
         normalized_time_vectors.append(normalized_layer)
     return normalized_time_vectors
--- toolbox.py
+++ toolbox.py
@@ -11,32 +11,31 @@ import numpy as np
 
 from neural_net import ObservableNet
 
-
-def initialize_data():
-    observable_net = ObservableNet(784)
-    # observable_net.add_layer(784, name='hidden')
-    observable_net.add_layer(20, name='hidden2')
-    observable_net.add_layer(10, name='output')
-    observable_net.train()
-
-    return observable_net.weights, observable_net.gradients, \
-           [observable_net.create_time_vectors('weight', layer) for layer in range(2)]
-
-
 class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.weights, self.gradients, self.time_vectors = initialize_data()
-        self.norm = normalize_time_vectors(self.time_vectors)
-        self.layer = 0
-        self.epoch = 0
+        self.time_vectors_gradients = None
+        self.time_vectors_weights = None
+        self.weights = None
+        self.gradients = None
+        self.initialize_observable_net()
+        self.layer = self.epoch = 0
         self.vis = 'gradient'
-        # self.visualize_time_vectors(1)
         self.figure = Figure()
         self.canvas = FigureCanvasQTAgg(self.figure)
         self.toolbar = NavigationToolbar2QT(self.canvas, self)
-        normalize_time_vectors(self.time_vectors)
         self.init_ui()
+
+    def initialize_observable_net(self):
+        observable_net = ObservableNet(784)
+        observable_net.add_layer(784, name='hidden')
+        observable_net.add_layer(10, name='output', activation='linear')
+        observable_net.train()
+
+        self.weights = observable_net.weights
+        self.gradients = observable_net.gradients
+        self.time_vectors_gradients = [observable_net.create_time_vectors('gradient', layer) for layer in range(2)]
+        self.time_vectors_weights = [observable_net.create_time_vectors('weight', layer) for layer in range(2)]
 
     def init_ui(self):
         self.setMinimumHeight(400)
@@ -109,13 +108,39 @@ class Window(QtWidgets.QWidget):
         layer_selection.addItems(self.weights['layer'].apply(str).unique())
         layer_selection.currentIndexChanged.connect(self.change_layer)
 
-        epoch_label = QtWidgets.QLabel('Epoch:')
-        layout.addWidget(epoch_label)
-        epoch_selection = QtWidgets.QComboBox()
-        layout.addWidget(epoch_selection)
+        l1_selection = QtWidgets.QGroupBox()
+        l1_selection_box = QtWidgets.QHBoxLayout()
+        l1_from_selection = QtWidgets.QLineEdit('0')
+        l1_to_selection = QtWidgets.QLineEdit('784')
+        l1_selection.setLayout(l1_selection_box)
+        l1_selection_box.addWidget(l1_from_selection)
+        l1_selection_box.addWidget(QtWidgets.QLabel(':'))
+        l1_selection_box.addWidget(l1_to_selection)
 
-        epoch_selection.addItems(self.weights['epoch'].apply(str).unique())
-        epoch_selection.currentIndexChanged.connect(self.change_epoch)
+        l2_selection = QtWidgets.QGroupBox()
+        l2_selection_box = QtWidgets.QHBoxLayout()
+        l2_from_selection = QtWidgets.QLineEdit('0')
+        l2_to_selection = QtWidgets.QLineEdit('784')
+        l2_selection.setLayout(l2_selection_box)
+        l2_selection_box.addWidget(l2_from_selection)
+        l2_selection_box.addWidget(QtWidgets.QLabel(':'))
+        l2_selection_box.addWidget(l2_to_selection)
+
+        layout.addWidget(QtWidgets.QLabel('Show Neurons:'))
+        layout.addWidget(QtWidgets.QLabel('Neurons of Layer 0:'))
+        layout.addWidget(l1_selection)
+        layout.addWidget(QtWidgets.QLabel('Neurons of Layer 1:'))
+        layout.addWidget(l2_selection)
+
+        # epoch_label = QtWidgets.QLabel('Epoch:')
+        # layout.addWidget(epoch_label)
+        # epoch_selection = QtWidgets.QComboBox()
+        # layout.addWidget(epoch_selection)
+
+        # epoch_selection.addItems(self.weights['epoch'].apply(str).unique())
+        # epoch_selection.currentIndexChanged.connect(self.change_epoch)
+
+        from_to1 = QtWidgets.QLabel
 
     def plot_heatmap(self):
         ax = self.figure.add_subplot(111)
@@ -133,20 +158,28 @@ class Window(QtWidgets.QWidget):
         self.canvas.draw()
 
     def plot(self):
+        self.figure.clear()
         ax = self.figure.add_subplot(111)
         ax.clear()
         a = list()
-        epochs = len(self.norm[1][0][0])
-        for row in self.norm[1]:
+        if self.vis == 'gradient':
+            time_vectors = self.time_vectors_gradients
+        else:
+            time_vectors = self.time_vectors_weights
+        epochs = len(time_vectors[self.layer][0][0])
+        for row in time_vectors[self.layer]:
             x = row.flatten()
             a.append(x)
         a = np.array(a)
         ax.yaxis.set_major_locator(MaxNLocator(integer=True))
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-        ax.grid(b=True, which='major', color='k', axis='x')
+        ax.set_xlabel('Layer ' + str(self.layer))
+        ax.set_ylabel('Layer ' + str(self.layer - 1))
+        ax.grid(b=True, which='major', color='k', axis='x', linestyle='--')
         ax.yaxis.set_minor_locator(plt.MultipleLocator(1))
         ax.xaxis.set_major_formatter(plt.FormatStrFormatter('%.0f'))
         ax.imshow(a, aspect='auto', cmap='RdGy', interpolation='None', extent=[0, int(len(a[0]) / epochs), len(a), 0])
+        ax
         self.canvas.draw()
 
     def visualize_time_vectors(self, layer):
